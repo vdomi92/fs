1ddf34f6fda3c8cc9abc16a6b6c17ca823472281
Domi <vataidomonkos@gmail.com>
2024-03-17 18:46:44 +0100

    changed project template

--- education/education.Server/Controllers/WeatherForecastController.cs
+++ education/education.Server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace backend.Controllers
+namespace education.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- education/education.Server/WeatherForecast.cs
+++ education/education.Server/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace backend
+namespace education.Server
 {
     public class WeatherForecast
     {
