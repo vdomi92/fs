e20588e5f37722c7c82b17917083575d32c2968b
Domi <vataidomonkos@gmail.com>
2024-04-06 00:24:23 +0200

    add repository skeleton

---
+++ education/data_access_layer/Domain/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using data_access_layer.Domain.Entites.Common;
+
+namespace data_access_layer.Domain.Repositories
+{
+    public interface IRepository<T> where T : Entity
+    {
+        public Task<T> GetById(int id);
+
+        public Task<IEnumerable<T>> GetAll();
+
+        public Task Create(T entity);
+
+        public Task Update(T entity);
+
+        public Task Delete(int id);
+
+        public Task SaveChanges();
+    }
+}
