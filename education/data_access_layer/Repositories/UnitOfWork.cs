3ef77a394dce818b2afbc72d0094550338444327
Domi <vataidomonkos@gmail.com>
2024-04-10 12:37:17 +0200

    unit of work

---
+++ education/data_access_layer/Repositories/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using data_access_layer.Contracts;
+using data_access_layer.Domain.Context;
+using data_access_layer.Domain.Entites.Common;
+using System.Data.Common;
+
+namespace data_access_layer.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _context;
+
+        private Dictionary<Type, object> _repositories;
+
+        private bool disposed = false;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            _repositories = [];
+        }
+
+        public IRepository<T> GetRepository<T>() where T : Entity
+        {
+            Type entityType = typeof(T);
+            if (!_repositories.ContainsKey(entityType))
+            {
+                _repositories[entityType] = new Repository<T>(_context);
+            }
+
+            return (IRepository<T>)_repositories[entityType];
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
