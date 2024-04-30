9ab85c61c7e82bfd1e07eded62b019b4f2314932
Domi <vataidomonkos@gmail.com>
2024-04-30 08:35:47 +0200

    models rework

--- education/data_access_layer/Domain/Entities/Common/User.cs
+++ education/data_access_layer/Domain/Entities/Common/User.cs
@@ -1,12 +1,9 @@
-﻿using data_access_layer.Domain.Entities.Classrooms;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace data_access_layer.Domain.Entites.Common
 {
     public class User : IdentityUser
     {
-        public List<Class> TeacherInClasses { get; set; } = [];
 
-        public List<Class> StudentInClasses { get; set; } = [];
     }
 }
--- education/data_access_layer/Domain/Entities/Quizzes/Attempt.cs
+++ education/data_access_layer/Domain/Entities/Quizzes/Attempt.cs
@@ -1,13 +1,10 @@
 ﻿using data_access_layer.Domain.Entites.Common;
-using data_access_layer.Domain.Entities.Classrooms;
 
 namespace data_access_layer.Domain.Entities.Quizzes
 {
     public class Attempt : Entity
     {
         public required User User { get; set; }
-
-        public required HomeWork HomeWork { get; set; }
 
         public required DateTime AnsweredAt { get; set; }
 
